b5a68f1888d1effaadbcead16a8c300d26fd442c
SerManish <cse.18bcs2830@gmail.com>
2020-05-24 00:06:34 +0530

    basic components implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe("A Test Recipe","a tasty dish","https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg"),
+    new Recipe("A Test Recipe","a tasty dish","https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {

db69737c117b9d15446f01a359f0558be1721ef3
SerManish <cse.18bcs2830@gmail.com>
2020-05-29 20:34:17 +0530

    basic routing complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailDefaultComponent } from './recipes/recipe-detail/recipe-detail-default/recipe-detail-default.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes:Routes = [
     {path:'',redirectTo:'/recipe',pathMatch:'full'},
     {path:'recipe',component:RecipesComponent,children:[
         {path:'',component:RecipeDetailDefaultComponent},
-        {path:':id',component:RecipeDetailComponent}
+        {path:'new',component:RecipeEditComponent},
+        {path:':id',component:RecipeDetailComponent},
+        {path:':id/edit',component:RecipeEditComponent}
     ]},
     {path:'shoppinglist',component:ShoppingListComponent}
 ];
